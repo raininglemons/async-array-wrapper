fa14829cf3445fcf30e2bf621a3fc1e1d554f30a
Dom England <dom@raininglemons.com>
2016-05-19 13:07:16 +0100

    added airbnb eslint

--- index.js
+++ index.js
@@ -1,39 +1,115 @@
+function AsyncArray(src, meta) {
+  const clone = src.slice(0);
+  if (!(clone instanceof AsyncArray)) {
+    clone.__proto__ = AsyncArray.prototype;
+  }
+  if (meta) {
+    clone.__running__ = meta.__running__;
+    clone.__async_queue__ = meta.__async_queue__;
+  }
+  return clone;
+}
 
-const AsyncArray = require('./source.js');
-const { async } = AsyncArray.utils;
-/*  TEST  */
+AsyncArray.prototype.__proto__ = Array.prototype;
 
-AsyncArray(['one', 'two', 'three', 'four'])
-  .forEach((done, val, i, arr) => {
-    console.log(val);
-    async(() => {
-      if (val === 'three')
-        arr.splice(i, 1);
-      done();
-    });
-  })
-  .forEach((done, val, i, arr) => {
-    console.log(val);
-    async(() => {
-      if (val === 'one')
-        arr.splice(i, 1);
-      done();
-    });
-  })
-  .map((done, val, i) => {
-    console.log('mapping');
-    done(`${i} -> ${val}`);
-  })
-  .then(function(done) {
-    console.log('final array');
-    console.log(this);
-    done([1,2,3,4]);
-  })
-  .then(function(done) {
-    console.log('and then again');
-    console.log(this);
-  });
+AsyncArray.prototype.__running__ = false;
 
-console.log('===============');
-console.log('SCRIPT FINISHED EXECUTING');
-console.log('===============');
+AsyncArray.prototype.__async_queue__ = [];
+
+/**
+ * Queues up a function to be executed asynchronously
+ * @param cb {Callback}
+ */
+function async(cb) {
+  process.nextTick(cb);
+}
+
+/**
+ * Starts the next queued method on the array object
+ */
+function nextInQueue() {
+  this.__running__ = false;
+  if (this.__async_queue__.length) {
+    if (!this.__running__ && this.__async_queue__.length) {
+      const { fn, args } = this.__async_queue__.splice(0, 1)[0];
+      fn.apply(this, args);
+    }
+  }
+}
+
+/**
+ * Generates a wrapper function for a native Array.prototype function
+ * @param superFn {Function}
+ * @returns {iterableFn}
+ */
+function generateIterableWrapper(superFn) {
+  return function iterableFn(callback, ...extraArgs) {
+    /*
+     If already waiting on a previous function to finish, queue up this
+     call instead of executing it.
+     */
+    if (this.__running__) {
+      this.__async_queue__.push({ fn: iterableFn, args: [callback].concat(extraArgs) });
+    } else {
+      this.__running__ = true;
+      /*
+       Instead of using a counter to detect how many done() functions have
+       been called, instead keep an array of each instance. This avoids any
+       problems if a done() instance is called accidentally more than once.
+       */
+      const doneFn = [];
+      const responses = [];
+      let i = 0;
+      const me = this;
+
+      superFn.apply(this, [function (...args) {
+        const ii = i++;
+        const done = (val) => async(() => {
+          const index = doneFn.indexOf(done);
+          if (index > -1) {
+            responses[ii] = val;
+            doneFn.splice(index, 1);
+            if (doneFn.length === 0) {
+              let iii = 0;
+              const result = superFn.call(me, () => responses[iii++]);
+              nextInQueue.call(result ? new AsyncArray(result, me) : me);
+            }
+          }
+        });
+
+        doneFn.push(done);
+
+        return callback.apply(this, [done].concat(args));
+      }].concat(extraArgs));
+    }
+    return this;
+  };
+}
+
+AsyncArray.prototype.forEach = generateIterableWrapper(Array.prototype.forEach);
+AsyncArray.prototype.map = generateIterableWrapper(Array.prototype.map);
+AsyncArray.prototype.every = generateIterableWrapper(Array.prototype.every);
+AsyncArray.prototype.filter = generateIterableWrapper(Array.prototype.filter);
+AsyncArray.prototype.find = generateIterableWrapper(Array.prototype.find);
+AsyncArray.prototype.findIndex = generateIterableWrapper(Array.prototype.findIndex);
+AsyncArray.prototype.reduce = generateIterableWrapper(Array.prototype.reduce);
+AsyncArray.prototype.reduceRight = generateIterableWrapper(Array.prototype.reduceRight);
+AsyncArray.prototype.some = generateIterableWrapper(Array.prototype.some);
+
+function then(callback, ...args) {
+  if (this.__running__) {
+    this.__async_queue__.push({ fn: then, args: [callback].concat(args) });
+  } else {
+    const done = (resp) => {
+      nextInQueue.call(resp ? new AsyncArray(resp) : this);
+    };
+    callback.call(this, done);
+  }
+  return this;
+}
+
+AsyncArray.prototype.then = then;
+
+AsyncArray.utils = { async };
+
+module.exports = AsyncArray;
---
+++ test.js
@@ -0,0 +1,41 @@
+
+const AsyncArray = require('./index.js');
+const { async } = AsyncArray.utils;
+/*  TEST  */
+
+AsyncArray(['one', 'two', 'three', 'four'])
+  .forEach((done, val, i, arr) => {
+    console.log(val);
+    async(() => {
+      if (val === 'three') {
+        arr.splice(i, 1);
+      }
+      done();
+    });
+  })
+  .forEach((done, val, i, arr) => {
+    console.log(val);
+    async(() => {
+      if (val === 'one') {
+        arr.splice(i, 1);
+      }
+      done();
+    });
+  })
+  .map((done, val, i) => {
+    console.log('mapping');
+    done(`${i} -> ${val}`);
+  })
+  .then(function (done) {
+    console.log('final array');
+    console.log(this);
+    done([1,2,3,4]);
+  })
+  .then(function (done) {
+    console.log('and then again');
+    console.log(this);
+  });
+
+console.log('===============');
+console.log('SCRIPT FINISHED EXECUTING');
+console.log('===============');
